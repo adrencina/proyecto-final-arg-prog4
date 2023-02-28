504e13ac1348fc96c47be80e018d46cedebda05c
Adr.Encina <adr.encina@gmail.com>
2023-02-28 02:36:28 -0300

    arreglos, cambios y reformas. seguimos!

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
+const menu = document.querySelector("#menuHeader");
+const menuOffsetTop = menu.offsetTop;
 
-/* variables */
-let icono = document.getElementsByClassName("iconoInfo");
+function fixMenu() {
+    if (window.pageYOffset >= menuOffsetTop){
+        menu.classList.add('menu-fijo');
+        menu.style.position = 'fixed';
+        menu.style.top = '0';
+    } else {
+        menu.classList.remove('menu-fijo');
+        menu.style.position = 'absolute';
+        menu.style.top = '80vh';
+    }
+}
 
-
+window.addEventListener('scroll', fixMenu);
